62965eda33e24ef925febc8998081c9a381eea69
Philip Krag Nielsen <philipkragnielsen@gmail.com>
2023-12-02 16:38:10 +0100

    Finish custom js action

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,18 @@
 import core from '@actions/core';
-import github from '@actions/github';
 import exec from '@actions/exec';
 
 function run() {
-    core.notice('Hello World!');
+    // Get some input values
+    const bucketName = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // Upload files to AWS
+    const s3Uri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    const websiteUrl = `http://${bucketName}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
